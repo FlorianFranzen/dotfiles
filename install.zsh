33443736f79dee052be7b9e13d1de932f9dd5636
Florian Franzen <Florian.Franzen@gmail.com>
2016-09-07 17:32:08 +0200

    Add install script

---
+++ install.zsh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+source $HOME/.homesick/repos/homeshick/homeshick.sh
+
+homeshick clone --batch robbyrussell/oh-my-zsh
+homeshick clone --batch trogdoro/xiki
+
+homeshick clone git@github.com:FloFra/dotfiles
