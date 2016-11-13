a0ef9c740ebbb6b2c961ac30c750643e3c00c325
Florian Franzen <Florian.Franzen@gmail.com>
2016-11-13 22:06:44 +0100

    Add git subrepo

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
 homeshick clone --batch robbyrussell/oh-my-zsh
+homeshick clone --batch ingydotnet/git-subrepo
 homeshick clone --batch trogdoro/xiki
 
 homeshick clone git@github.com:FloFra/dotfiles
