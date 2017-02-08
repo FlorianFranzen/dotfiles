13f86114c63cdcb4eebc7c079e774ec116e5d838
Florian Franzen <Florian.Franzen@gmail.com>
2017-02-08 23:26:34 +0100

    Add spacemacs symlink and to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
 homeshick clone --batch robbyrussell/oh-my-zsh
+homeshick clone --batch syl20bnr/spacemacs
 homeshick clone --batch ingydotnet/git-subrepo
 homeshick clone --batch trogdoro/xiki
 
