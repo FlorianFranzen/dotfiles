9c2e6318df2f8ce0cfe3921829880969f8a586fc
Florian Franzen <Florian.Franzen@gmail.com>
2020-04-14 15:16:18 +0200

    nix-call: passthrough additional args

--- home/.zshrc.d/nixos.zsh
+++ home/.zshrc.d/nixos.zsh
@@ -1,3 +1,7 @@
 alias nixos-env="sudo nix-env -p /nix/var/nix/profiles/system"
 
-function nix-call() nix-build -E "with import <nixpkgs> {}; callPackage $1 {}"
+function nix-call() {
+  nixfile="$1"
+  shift
+  nix-build -E "with import <nixpkgs> {}; callPackage $nixfile {}" $@
+}
