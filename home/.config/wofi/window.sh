66eec834735b526f31751dd54b2965c8f854e3d4
Florian Franzen <Florian.Franzen@gmail.com>
2021-04-22 20:10:13 +0200

    wofi: extend window selection info

--- home/.config/wofi/window.sh
+++ home/.config/wofi/window.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name)' | wofi --show dmenu | {
+swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " - " + .name + " - " + .shell )' | wofi --show dmenu | {
   read -r id name
   swaymsg "[con_id=$id]" focus
 }
