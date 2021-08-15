40648d81f4ff350d7eaf6e9b6b9dc2d8d66f917c
Florian Franzen <Florian.Franzen@gmail.com>
2021-08-15 11:37:50 +0200

    add patches for mako, swaylock and workstyle

--- home/.config/nixpkgs/config.nix
+++ home/.config/nixpkgs/config.nix
@@ -2,6 +2,37 @@
   allowUnfree = true;
 
   packageOverrides = pkgs: {
+    swaylock-effects = pkgs.swaylock-effects.overrideAttrs (old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "mortie";
+        repo = "swaylock-effects";
+        rev = "80421107675e8cc9eb24502339dad079b83818e4";
+        sha256 = "0arkvdwidww3pl89hkl13lii9pj14rslx7lh5wc9sssbymp6qijy";
+      };
+    });
+
+    workstyle = pkgs.workstyle.overrideAttrs (old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "pierrechevalier83";
+        repo = old.pname;
+        rev = "f2023750d802259ab3ee7d7d1762631ec157a0b1";
+        sha256 = "04xds691sw4pi2nq8xvdhn0312wwia60gkd8b1bjqy11zrqbivbx";
+      };
+      cargoSha256 = "0xwv8spr96z4aimjlr15bhwl6i3zqp7jr45d9zr3sbi9d8dbdja2";
+    });
+
+    mako = pkgs.mako.overrideAttrs (old: rec {
+      name = "mako-${version}";
+      version = "1.6";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "emersion";
+        repo = old.pname;
+        rev = "v${version}";
+        sha256 = "sha256-RcPwZ5NeO9vxwPWfgj5x3wVdCYGKumnYT3ngHEAWfW0=";
+      };
+    });
+
     ncmpcppWithEyeCandy = pkgs.ncmpcpp.override { visualizerSupport = true; };
 
     # Full featured version of opencv3
